c67d120a749f73542d7885eb8534ab833f45c856
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-07-09 20:01:33 +0530

    getAuthToken

---
+++ client/src/utils/getAuthToken.jsx
@@ -0,0 +1,6 @@
+export default function getAuthToken(){
+    const data = JSON.parse(localStorage.getItem("auth"));
+    const role = localStorage.getItem("role");
+
+    return data && role ? {...data, role:role} : null;
+}
