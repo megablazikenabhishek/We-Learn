a1b38d8065c1dc9f2b985e3bad731aaf3886aebd
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2023-07-10 00:46:06 +0530

    final

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const app = express();
 //middlewares
 app.use(require("cors")())
 app.use(express.json());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 //routes
